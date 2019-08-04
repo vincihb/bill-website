74610e7ca1af4b0dd2434c7f89ba2a1d16caf81c
Michael Albinson <mikealbinson@gmail.com>
2019-08-04 16:54:26 -0700

    Tabs -> spaces

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, send_from_directory, request
+from flask import Flask, send_from_directory, request
 from Souper import get_missing_img
 from client.TemplateManager.TemplateManager import template_manager
 from config.ConfReader import conf
@@ -9,58 +9,58 @@ app.debug = True
 
 @app.route("/", methods=['GET'])
 def index_route():
-	return template_manager.get_template('index.html')
+    return template_manager.get_template('index.html')
 
 
 @app.route("/about", methods=['GET'])
 def about_route():
-	return template_manager.get_template('about.html')
+    return template_manager.get_template('about.html')
 
 
 @app.route("/filter", methods=['GET'])
 def filter_route():
-	return template_manager.get_template('filter.html')
+    return template_manager.get_template('filter.html')
 
 
 @app.route("/result", methods=['POST'])
 def get_result_and_send_new_title():
-	global m
-	data = request.get_json()
-	if data['init']:
-		return m.get_next()
-	else:
-		m.set_result(data['res'])
-		return m.get_next()
+    global m
+    data = request.get_json()
+    if data['init']:
+        return m.get_next()
+    else:
+        m.set_result(data['res'])
+        return m.get_next()
 
 
 @app.route("/missing", methods=['POST'])
 def get_missing():
-	data = request.get_json()
-	if 'first' in data and 'last' in data:
-		return get_missing_img(data['first'], data['last'])
-	else:
-		print('information missing from request')
-		return ''
+    data = request.get_json()
+    if 'first' in data and 'last' in data:
+        return get_missing_img(data['first'], data['last'])
+    else:
+        print('information missing from request')
+        return ''
 
 
 @app.route('/js/<path:path>')
 def send_js(path):
-	return send_from_directory('client/static/js', path)
+    return send_from_directory('client/static/js', path)
 
 
 @app.route('/css/<path:path>')
 def send_css(path):
-	return send_from_directory('client/static/css', path)
+    return send_from_directory('client/static/css', path)
 
 
 @app.route('/assets/<path:path>')
 def send_asset(path):
-	return send_from_directory('client/static/assets', path)
+    return send_from_directory('client/static/assets', path)
 
 
 @app.route('/ca/', methods=['GET'])
 def canadian_root():
-	return template_manager.get_template('index.html')
+    return template_manager.get_template('index.html')
 
 
 app.run(port=conf.PORT)
